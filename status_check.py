f6f5cb27c7963ca6bbe620993f0c1477f5aa8102
sanketbathia <sanket_bathia@yahoo.com>
2019-11-12 12:13:58 +0530

    Final code for URL health check with all environment

--- status_check.py
+++ status_check.py
@@ -4,25 +4,26 @@ from urllib.error import URLError
 import datetime
 
 def my_function():
-  print("Hello from a function")
-  #######################################################
-  userInput = int(input("Please Enter environment ID from below list for health check: "))
-
+  #Defined Values in dictionaries to get user inputs on which environment we want to run health check
   environment = {
     1: "iVerify_Dev",
     2: "iforms_UAT",
     3: "iforms_QA",
     4: "iforms_DEV",
     5: "iConductor_DEV",
-    6: "iConductor_UAT7",
+    6: "iConductor_UAT",
     7: "iConductor_QA2"}
+  #Print the dictionaries for user inputs
+  for key,value in environment.items():
+    print(key,"==>",value)
+  #Take user input into integer and pass that for pickup a file according to that
+  userInput = int(input("Please Enter environment ID from below list for health check: "))
   print(environment[userInput])
   filename = environment[userInput] + ".csv"
 
-  #########################################################
   source_path = "C:\\Users\\bathia_s\\Desktop\\"
   source_filename = environment[userInput] + ".csv"
-  dest_filename = environment[userInput] + str(datetime.datetime.now().strftime('%Y_%m_%d_%H_%M_%S')) + '.csv'
+  dest_filename = environment[userInput] + '_' + str(datetime.datetime.now().strftime('%Y_%m_%d_%H_%M_%S')) + '.csv'
   dest_path = "C:\\Users\\bathia_s\\Desktop\\"
 
   data = pd.read_csv(source_path + source_filename, encoding="ISO-8859-1")
@@ -53,5 +54,4 @@ def my_function():
 
   data.to_csv(dest_path + dest_filename)
 
-my_function()
 my_function()
